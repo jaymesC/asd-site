f73b0779a8a84b3b2f7ffe724a61201efcb4b4ba
James <clementjaymes@.com>
2021-04-22 21:36:57 -0700

    updated the Navbar component

---
+++ src/components/molecules/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+export default function Navbar() {
+  const Navbar = styled.nav`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin: 40px auto;
+  `
+  const Links = styled.div`
+  display: inline-block;
+  text-align: right;
+  `
+  const Navlinks = styled.a`
+  // display: inline-block;
+  margin-left: 50px;
+  font-weight: 400;
+  padding-bottom: 0px;
+  border-bottom: 3px solid transparent;
+  `
+  return (
+    <div>
+      <Navbar>
+      <h1>Gatsby Project</h1>
+      <Links>
+        <Navlinks to="/">Home</Navlinks>
+        <Navlinks to="/about">About</Navlinks>
+        <Navlinks to="/projects"> Projects</Navlinks>
+      </Links>
+    </Navbar>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
+import Navbar from '../components/molecules/Navbar';
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Navbar/>
+  ) 
+  
 }
