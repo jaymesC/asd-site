d88245f6f08bad837d2db9ebbfd501b5d4ed163c
James <clementjaymes@.com>
2021-04-23 17:14:01 -0700

    midified navbar component

--- src/components/molecules/Navbar.js
+++ src/components/molecules/Navbar.js
@@ -1,31 +1,40 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
+import "normalize.css"
 
 export default function Navbar() {
   const Navbar = styled.nav`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
+  display: flex;
+  justify-content:center;
   margin: 40px auto;
   `
   const Links = styled.div`
   display: inline-block;
-  text-align: right;
+
   `
   const Navlinks = styled.a`
   display: inline-block;
   margin-left: 50px;
-  font-weight: 400;
-  padding-bottom: 0px;
+  // font-weight: 200;
+  margin-top: 20px;
   border-bottom: 3px solid transparent;
+  font-size: 22px;
+  cursor:pointer;
+  
   `
   return (
     <div>
       <Navbar>
-      <h1>Gatsby Project</h1>
+      <img src="logo.JPG" alt=""/>
       <Links>
+      
+      <Navlinks></Navlinks>
+      
         <Navlinks to="/">Home</Navlinks>
         <Navlinks to="/about">About</Navlinks>
+        <Navlinks to="/projects"> Projects</Navlinks>
+        <Navlinks to="/projects"> Projects</Navlinks>
         <Navlinks to="/projects"> Projects</Navlinks>
       </Links>
     </Navbar>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
-
+import "normalize.css"
 import Navbar from '../components/molecules/Navbar';
 
 export default function Home() {
   return (
+    <>
     <Navbar/>
+    
+    </>
   ) 
   
 }
